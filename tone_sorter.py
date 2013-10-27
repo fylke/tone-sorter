b22238d229f14f673bcdfafbb6d27995c4e71961
Magnus Falk <magnus.falk@gmail.com>
2013-10-27 03:10:59 +0100

    Refactor the input parsing to be separate

--- tone_sorter.py
+++ tone_sorter.py
@@ -60,25 +60,32 @@ def sanitize_file(original_file):
             f.close()
             return original_file
 
-def main(argv):
+def print_help():
+    print "tone_sorter.py -i <inputfile> -o <outputfile>"
+
+def parse_input(argv):
     inputfile = ""
     outputfile = ""
     try:
         opts, args = getopt.getopt(argv, 'hi:o:', ['ifile=','ofile='])
     except getopt.GetoptError:
-        print 'tone_sorter.py -i <inputfile> -o <outputfile>'
+        print_help()
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'tone_sorter.py -i <inputfile> -o <outputfile>'
+            print_help()
             sys.exit()
         elif opt in ('-i', '--ifile'):
             inputfile = arg
         elif opt in ('-o', '--ofile'):
             outputfile = arg
     if inputfile == "" or outputfile == "":
-        print 'tone_sorter.py -i <inputfile> -o <outputfile>'
+        print_help()
         sys.exit(2)
+    return (inputfile, outputfile)
+
+def main(argv):
+    (inputfile, outputfile) = parse_input(argv)
 
     phrases = []
     cleaned_file = ""
